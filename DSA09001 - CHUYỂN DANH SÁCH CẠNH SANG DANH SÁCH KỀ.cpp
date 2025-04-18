c276f902366a4b52a9844b034fb3a4aecbcf8d63
Trần Thị Ánh Dương <177900937+tad315@users.noreply.github.com>
2025-04-18 14:57:02 +0700

    Create DSA09001 - CHUYỂN DANH SÁCH CẠNH SANG DANH SÁCH KỀ.cpp

---
+++ DSA09001 - CHUYỂN DANH SÁCH CẠNH SANG DANH SÁCH KỀ.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+
+int main()
+{
+    fast;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int v, e;
+        cin >> v >> e;
+
+        vector<vector<int>> adj(v + 1);
+        for (int i = 0; i < e; ++i)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        for (int i = 1; i <= v; ++i)
+        {
+            cout << i << ": ";
+            sort(adj[i].begin(), adj[i].end());
+            for (auto x : adj[i]) cout << x << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
