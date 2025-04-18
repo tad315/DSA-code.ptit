b4da6a410fd87f073de7346698237b0294b5812f
Trần Thị Ánh Dương <177900937+tad315@users.noreply.github.com>
2025-04-18 18:49:15 +0700

    Create DSA09024 - BFS TRÊN ĐỒ THỊ CÓ HƯỚNG.cpp

---
+++ DSA09024 - BFS TRÊN ĐỒ THỊ CÓ HƯỚNG.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+
+int v, e, start;
+vector<int> adj[1001];
+bool visited[1001];
+
+void bfs(int u)
+{
+    queue<int> q;
+    q.push(u);
+    visited[u] = 1;
+    while (!q.empty())
+    {
+        int v = q.front();
+        q.pop();
+        cout << v << " ";
+        for (int x : adj[v])
+        {
+            if (!visited[x])
+            {
+                q.push(x);
+                visited[x] = 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    fast;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        for (int i = 0; i < 1001; i++)
+            adj[i].clear();
+        memset(visited, 0, sizeof(visited));
+        cin >> v >> e >> start;
+        for (int i = 0; i < e; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        for (int i = 1; i <= v; i++)
+            sort(adj[i].begin(), adj[i].end());
+        bfs(start);
+        cout << endl;
+    }
+    return 0;
+}
