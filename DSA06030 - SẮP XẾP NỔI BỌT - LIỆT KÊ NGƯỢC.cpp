1d9913c82214eb25208776da5375774c69dd5b26
Trần Thị Ánh Dương <177900937+tad315@users.noreply.github.com>
2025-07-25 09:34:22 +0700

    Update DSA06030 - SẮP XẾP NỔI BỌT - LIỆT KÊ NGƯỢC.cpp

--- DSA06030 - SẮP XẾP NỔI BỌT - LIỆT KÊ NGƯỢC.cpp
+++ DSA06030 - SẮP XẾP NỔI BỌT - LIỆT KÊ NGƯỢC.cpp
@@ -1,51 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void solve(vector<int> &a, int n)
-{
-    vector<vector<int>> steps;
-    bool check;
-
-    for (int i = 0; i < n; i++)
-    {
-        check = 0;
-        for (int j = 0; j < n - 1; j++)
-            if (a[j] > a[j + 1])
-            {
-                swap(a[j], a[j + 1]);
-                check = 1;
-            }
-
-        if (!check)
-            break;
-        steps.push_back(a);
-    }
-
-    for (int i = steps.size() - 1; i >= 0; i--)
-    {
-        cout << "Buoc " << i + 1 << ": ";
-        for (int x : steps[i])
-        {
-            cout << x << " ";
-        }
-        cout << endl;
-    }
-}
+#define boost ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
 
 int main()
 {
-    int t;
+    boost;
+    int t, n;
     cin >> t;
     while (t--)
     {
-        int n;
         cin >> n;
         vector<int> a(n);
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        vector<vector<int>> steps;
+        for (int i = 0; i < n - 1; ++i)
         {
-            cin >> a[i];
+            int check = 0;
+            for (int j = 0; j < n - 1; ++j)
+            {
+                if (a[j + 1] < a[j])
+                {
+                    swap(a[j], a[j + 1]);
+                    check = 1;
+                }
+            }
+            if (check) steps.pb(a);
+            
         }
-        solve(a, n);
+        for (int i = steps.size() - 1; i >= 0; --i)
+        {
+            cout << "Buoc " << i + 1 << ": ";
+            for (int x : steps[i]) cout << x << " ";
+            cout << endl;
+        }
     }
     return 0;
 }
