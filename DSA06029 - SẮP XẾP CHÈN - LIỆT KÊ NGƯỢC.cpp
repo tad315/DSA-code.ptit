8a43194e48cf3bff01bb356e35ecaf3d6e6eb372
Trần Thị Ánh Dương <177900937+tad315@users.noreply.github.com>
2025-07-25 09:15:53 +0700

    Update DSA06029 - SẮP XẾP CHÈN - LIỆT KÊ NGƯỢC.cpp

--- DSA06029 - SẮP XẾP CHÈN - LIỆT KÊ NGƯỢC.cpp
+++ DSA06029 - SẮP XẾP CHÈN - LIỆT KÊ NGƯỢC.cpp
@@ -1,37 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define boost ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
 
-void solve(vector<int> &a, int n) {
-    vector<vector<int>> steps;
-    steps.push_back({a[0]});
-    
-    for (int i = 1; i < n; i++) {
-        int key = a[i];
-        int j = i - 1;
-        while (j >= 0 && a[j] > key) {
-            a[j + 1] = a[j];
-            j--;
-        }
-        a[j + 1] = key;
-        steps.push_back(vector<int>(a.begin(), a.begin() + i + 1));
-    }
-    
-    for (int i = steps.size() - 1; i >= 0; i--) {
-        cout << "Buoc " << i << ": ";
-        for (int x : steps[i]) {
-            cout << x << " ";
-        }
-        cout << endl;
-    }
-}
-
-int main() {
+int main()
+{
+    boost;
     int n;
     cin >> n;
     vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    vector<vector<int>> steps;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int tmp = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > tmp)
+        {
+            a[j + 1] = a[j];
+            --j;
+        }
+        a[j + 1] = tmp;
+        vector<int> cur;
+        for (int k = 0; k <= i; ++k) cur.pb(a[k]);
+        steps.pb(cur);
     }
-    solve(a, n);
+    for (int i = n - 1; i >= 0; --i)
+    {
+        cout << "Buoc " << i << ":";
+        for (int x : steps[i]) cout << " " << x;
+        cout << endl;
+    }
     return 0;
 }
