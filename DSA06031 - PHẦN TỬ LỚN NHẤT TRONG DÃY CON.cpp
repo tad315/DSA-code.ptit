d23702d754a684a76c4a538d619885481f5ec32f
Trần Thị Ánh Dương <177900937+tad315@users.noreply.github.com>
2025-07-27 00:00:08 +0700

    Update DSA06031 - PHẦN TỬ LỚN NHẤT TRONG DÃY CON.cpp

--- DSA06031 - PHẦN TỬ LỚN NHẤT TRONG DÃY CON.cpp
+++ DSA06031 - PHẦN TỬ LỚN NHẤT TRONG DÃY CON.cpp
@@ -1,34 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define boost ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
 
 int main()
 {
+    boost;
     int t;
     cin >> t;
     while (t--)
     {
         int n, k;
         cin >> n >> k;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++)
+        int a[n];
+        for (int i = 0; i < n; ++i)
             cin >> a[i];
-        deque<int> dp;
-        for (int i = 0; i < k; i++)
+        deque<int> dq;
+        for (int i = 0; i < n; ++i)
         {
-            while (!dp.empty() && a[i] >= a[dp.back()])
-                dp.pop_back();
-            dp.push_back(i);
+            if (!dq.empty() && dq.front() <= i - k)
+                dq.pop_front();
+            while (!dq.empty() && a[dq.back()] <= a[i])
+                dq.pop_back();
+            dq.push_back(i);
+            if (i >= k - 1)
+                cout << a[dq.front()] << " ";
         }
-        for (int i = k; i < n; i++)
-        {
-            cout << a[dp.front()] << " ";
-            while (!dp.empty() && dp.front() <= i - k)
-                dp.pop_front();
-            while (!dp.empty() && a[i] >= a[dp.back()])
-                dp.pop_back();
-            dp.push_back(i);
-        }
-        cout << a[dp.front()] << endl;
+        cout << endl;
     }
     return 0;
 }
